ed7cefd27094f0d9200f172dcf966838ab3b7aaf
Asif Ali <asifkhan3596073@gmail.com>
2024-06-24 10:24:25 +0530

    Complete service cards

--- src/Utils/constant.js
+++ src/Utils/constant.js
@@ -1,18 +1,41 @@
 export const headerLinks = [
-    {
-    links:"/",
-    item: "Home"
-    },
-    {
-    links:"/about",
-    item: "About us"
-    },
-    {
-    links:"/services",
-    item: "Services"
-    },
-    {
-    links:"/Contact",
-    item: "contact"
-    },
-]
+  {
+    links: "/",
+    item: "Home",
+  },
+  {
+    links: "/about",
+    item: "About us",
+  },
+  {
+    links: "/services",
+    item: "Services",
+  },
+  {
+    links: "/Contact",
+    item: "contact",
+  },
+];
+
+export const serviceCardContent = [
+  {
+    icons: "icons",
+    heading: "Web Development",
+    subHeading: "Creating websites using coding and design techniques.",
+  },
+  {
+    icons: "icons",
+    heading: "UI/UX Designing",
+    subHeading: "Expert Web Designing for Impactful Digital Experiences.",
+  },
+  {
+    icons: "icons",
+    heading: "Mobile Application",
+    subHeading: "Unleashing Innovation in the Palm of Your Hand.",
+  },
+  {
+    icons: "icons",
+    heading: "Digital Marketing",
+    subHeading: "Strategic Promotion for Online Triumph.",
+  },
+];
